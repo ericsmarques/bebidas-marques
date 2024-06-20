8048c57d9d2fc42378d95e6cbd3284d3ddfe5ca5
Eric Marques <ericss.marques@hotmail.com>
2024-06-20 20:39:43 -0300

    Alteração do parágrafo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Loja de Bebidas!
+          Loja de Bebidas em construção...
         </p>
         <a
           className="App-link"
