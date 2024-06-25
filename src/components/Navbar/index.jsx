40be86eb07af21fd19223ebbcbe2e484512f46bf
Eric Marques <ericss.marques@hotmail.com>
2024-06-25 18:03:48 -0300

    Criação de components e navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <div claasName="Navbar">
+            <h1>Loja de Bebidas</h1>
+
+            <nav>
+                <li><a target="_blank" href="https://www.linkedin.com" rel="noreferrer"> Sobre (loja exemplo)</a></li>
+                <li><a target="_blank" href="https://www.mundodasbebidas.com.br/" rel="noreferrer"> Consulte nossos produtos</a></li>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
